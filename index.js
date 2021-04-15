6f51ea78294ef31485056dc9e73ada71eaffa710
MONIKA SHARMA <monika2004sharma@gmail.com>
2021-04-15 20:58:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,9 +15,10 @@ const dbURL = process.env.dbURL
 
 const app = express()
 app.use(express.json())
-app.use(cors({
-    origin:"https://e-learning-application.netlify.app/"
-}))
+app.use(cors())
+// app.use(cors({
+//     origin:"https://e-learning-application.netlify.app/"
+// }))
 
 const port = process.env.PORT || 5000
 app.listen(port, () => console.log('your app is running wonderfully at', port))
